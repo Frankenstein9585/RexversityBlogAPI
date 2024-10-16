7c81d24a00fed496ce9ab7a66f9f35cff766ee6c
Donald Peters <donny9585@gmail.com>
2024-10-16 13:07:35 +0100

    Setup basic server

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log('Happy developing ✨')
+require('dotenv').config();
+
+const express = require('express');
+const server = express();
+const PORT = process.env.PORT || 3000;
+
+server.use(express.json());
+
+server.use('/posts', require('./routes/posts'));
+server.use('/users', require('./routes/users'));
+
+server.listen(PORT, () => {
+   console.log(`Server started on http://localhost:${PORT}`);
+});
